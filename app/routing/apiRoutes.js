d5926019cf11cc9795ecabfabe9dcabbabe124be
mccarthinator <mccarthy.cath@gmail.com>
2017-08-13 23:49:00 -0500

    adding modal

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+// API Routes
+
+var friendsArray = require("../data/friends.js");
+
+module.exports = function(app) {
+  
+  //return the friends array
+  app.get("/api/friends", function(req, res){
+    res.json(friendsArray);
+  });
