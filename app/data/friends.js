a73e94575d3b421940587a864100fbddf5e832bc
mccarthinator <mccarthy.cath@gmail.com>
2017-08-14 10:45:08 -0500

    adding tester friends to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsArray = [{
-    "name":"tester",
-    "photo":"/yearbook-pic.jpg",
+    "name":"tester1",
+    "photo":"http://images.gawker.com/slblxlpcde6vtfu39z6c/original.jpg",
     "scores":[
         1,
         3,
@@ -13,7 +13,43 @@ var friendsArray = [{
         3,
         5
       ]
-  }];
+  },
+{
+    "name":"tester2",
+    "photo":"http://media4.s-nbcnews.com/j/newscms/2016_20/1089741/celeb-yearbook-jimmy-fallon-today-160516_4d4b12a57681ee3e35cdbc749626cc52.today-ss-slide-desktop.jpg",
+    "scores":[
+        1,
+        2,
+        3,
+        4,
+        5,
+        1,
+        2,
+        3,
+        4,
+        5
+      ]
+},
+{
+    "name":"tester3",
+    "photo":"https://4.bp.blogspot.com/-L70oPcqYjy0/VbdfSnNyd1I/AAAAAAABr4g/_J5OeqIGjqM/s1600/Yearbook%2B1980s%2B%25284%2529.jpg",
+    "scores":[
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1
+      ]
+
+}
+
+
+];
   
   // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friendsArray;
