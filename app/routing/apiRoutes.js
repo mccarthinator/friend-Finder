369d32457539c39fb9758cb607e76ba6ce816a8b
mccarthinator <mccarthy.cath@gmail.com>
2017-08-14 11:55:48 -0500

    survey.html edits

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,5 +9,21 @@ module.exports = function(app) {
     res.json(friendsArray);
   });
 
+   //this takes in the incoming data from survey
   app.post("/api/friends", function(req, res) {
     var newUser = req.body;
+    //this needs to change their answers into number values
+    newUser.scores = newUser.scores.??? 
+
+    ///// next, need to calculate absolute value difference between user scores ////
+    function findAbsoluteValue(){
+    };
+
+    // find lowest absolute value score ////
+    var lowestAbsVal = "";
+
+    //// need a function that adds new user to the friendsArray
+
+    ////need a function that returns the user which has lowest abs value score to the modal //////
+
+    ///call that function ^ ////
