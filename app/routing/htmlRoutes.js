7facb92ffbdbb056387852ffae86fefeca76b31c
mccarthinator <mccarthy.cath@gmail.com>
2017-08-13 20:32:36 -0500

    fixing module.exports function

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,8 +11,9 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get('/survey', function (req, res) {
-      res.render('survey', {});
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
+  
   app.use('/home', function (req, res) {
       res.render('home', {
           // superHeroList: superHeroList
