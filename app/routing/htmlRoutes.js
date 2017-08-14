028d394b3d2ec966e168aed8813e1ebb2c65256f
mccarthinator <mccarthy.cath@gmail.com>
2017-08-13 20:31:23 -0500

    html routes setup

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,9 +2,14 @@
 // A GET Route to /survey which should display the survey page.
 // A default USE route that leads to home.html which displays the home page.
 
+
+// dependencies //
+var path = require("path");
+
+
 // Routes
 
-function defineHTMLRoutes(app, ?) {
+module.exports = function(app) {
   app.get('/survey', function (req, res) {
       res.render('survey', {});
   });
