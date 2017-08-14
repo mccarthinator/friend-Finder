b383a44ea86d78029fae66d28a60e12e4a9ec80a
mccarthinator <mccarthy.cath@gmail.com>
2017-08-13 20:28:31 -0500

    updating friends.js with array

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsArray = [{
+    "name":"tester",
+    "photo":"/yearbook-pic.jpg",
+    "scores":[
+        1,
+        3,
+        5,
+        1,
+        2,
+        5,
+        1,
+        4,
+        3,
+        5
+      ]
+  }];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
